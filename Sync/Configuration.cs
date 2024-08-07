b28eb272acf5883b72061ee51f24880a0bce1b5f
joyandrews <joyleeandrews@gmail.com>
2024-08-06 22:42:26 -0400

    Fix config

--- Sync/Configuration.cs
+++ Sync/Configuration.cs
@@ -18,8 +18,8 @@ namespace Sync
             Values = new Dictionary<string, string>()
             {
                 { "VirtuousApiBaseUrl", "https://api.virtuoussoftware.com" },
-                { "VirtuousApiKey", "v_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiN2VhYTBhNTQtYTBiZC00OTNlLWFjNDMtZjNjZGEwZmVlNWQ5IiwiZXhwIjoyMTQ3NDgzNjQ3LCJpc3MiOiJodHRwczovL2FwcC52aXJ0dW91c3NvZnR3YXJlLmNvbSIsImF1ZCI6Imh0dHBzOi8vYXBpLnZpcnR1b3Vzc29mdHdhcmUuY29tIn0.oN0bfmYMS7lPxGtVH3ouEVhD0Kuzoqa2nAnuvPTyPpk" },
-                { "ConnectionString", "REPLACE_WITH_API_KEY_PROVIDED" }
+                { "VirtuousApiKey", "REPLACE_WITH_API_KEY_PROVIDED" },
+                { "ConnectionString", @"Data Source =(localdb)\MSSQLLocalDB; Initial Catalog =Virtuous; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False" }
             };
         }
 
